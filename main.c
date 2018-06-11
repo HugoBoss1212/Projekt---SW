41445bcdc404ec329a41dccfb1055a410fe25c19
Kacper <lechowicz2@gmail.com>
2018-06-11 11:31:09 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Projekt - SW.c
+ *
+ * Created: 11.06.2018 11:21:44
+ * Author : Kacper
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
