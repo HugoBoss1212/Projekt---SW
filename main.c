240c80381699d2cf1b3000aa4deb5a632d913892
Kacper <lechowicz2@gmail.com>
2018-06-13 12:30:46 +0200

    dodanie wypisywania do lcd

--- main.c
+++ main.c
@@ -3,20 +3,21 @@
  *
  * Created: 04.06.2018 11:21:44
  * Author : Kacper Lechowicz
- // Port B0 i B1 do switch S1 i S2
- // Port A2 to LCD_RS
- // Port A3 to LCD_E
- // Port A4 to LCD_D4
- // Port A5 to LCD_D5
- // Port A6 to LCD_D6
- // Port A7 to LCD_D7
+ * Port B0 i B1 do switch S1 i S2
+ * Port A2 to LCD_RS
+ * Port A3 to LCD_E
+ * Port A4 to LCD_D4
+ * Port A5 to LCD_D5
+ * Port A6 to LCD_D6
+ * Port A7 to LCD_D7
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "HD44780.h"
+#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #define KEY_NEXT (1<<PB0)
 #define KEY_PREVIOUS (1<<PB1)
@@ -26,67 +27,76 @@
 #define STR4 "Mode 4"
 #define STR5 "Mode 5"
 
-void obsluga_delay( uint16_t ms, uint8_t *mode );
-void print_to_LCD( const char* message);
+void obsluga_delay( uint16_t ms );
 
 uint16_t key_lock1=0, key_lock2=0;
+char* TAB_MODE[5] = {STR1, STR2, STR3, STR4, STR5};
 uint16_t delay = 50;
+uint16_t j = 0;
+uint8_t mode = 0;
 
 int main(void){
-	const char* TAB_MODE[5] = {STR1, STR2, STR3, STR4, STR5};
-	uint8_t mode = 0;
+	//key ports init
 	DDRB &= ~KEY_NEXT | ~KEY_PREVIOUS;
 	PORTB |= KEY_NEXT | KEY_PREVIOUS;
+
+	//lcd ports init
+	TCCR1B |= 1<<WGM12;
+	OCR1A = 4000;
+	TCCR1B |=1<<CS10 | 1<< CS12;
+	TIMSK |= 1<<OCIE1A;
+	LCD_Initalize();
+	sei();
 	 
     while (1){
 		switch (mode) {
 			case 0: {
-				print_to_LCD(TAB_MODE[mode]);
 			
-				obsluga_delay(delay, &mode);
+				obsluga_delay(delay);
 			}
 			case 1:{
-				print_to_LCD(TAB_MODE[mode]);
 			
-				obsluga_delay(delay, &mode);
+				obsluga_delay(delay);
 			}
 			case 2:{
-				print_to_LCD(TAB_MODE[mode]);
 			
-				obsluga_delay(delay, &mode);
+				obsluga_delay(delay);
 			}
 			case 3:{
-				print_to_LCD(TAB_MODE[mode]);
 			
-				obsluga_delay(delay, &mode);
+				obsluga_delay(delay);
 			}
 			case 4:{
-				print_to_LCD(TAB_MODE[mode]);
 			
-				obsluga_delay(delay, &mode);
+				obsluga_delay(delay);
 			}
 		}
     }
 }
 
 
-void obsluga_delay( uint16_t ms, uint8_t *mode) {
+void obsluga_delay( uint16_t ms ) {
 	while( ms-- ) {
 		if( !key_lock1 && !(PINA & KEY_NEXT) ) {
 			key_lock1 = 500;
-			*mode += 1;
-			if(*mode > 4){ *mode = 4; }
+			mode += 1;
+			if(mode > 4){ mode = 4; }
 		} else if( key_lock1 && (PINA & KEY_NEXT) ) key_lock1--;
 
 		if( !key_lock2 && !(PINA & KEY_PREVIOUS) ) {
 			key_lock2 = 500;
-			*mode -= 1;
-			if(*mode < 0){ *mode = 0; }
+			mode -= 1;
+			if(mode < 0){ mode = 0; }
 		} else if( key_lock2 && (PINA & KEY_PREVIOUS) ) key_lock2--;
 		_delay_ms(0.95);
 	}
 }
 
-void print_to_LCD( const char* message){
-	;
+ISR(TIMER1_COMPA_vect){
+	char buffor[16];
+	LCD_Clear();
+	for(uint8_t i = 0; i < 16; i++){
+		LCD_WriteData(TAB_MODE[mode][i+j]);
+	}
+	j++;
 }
